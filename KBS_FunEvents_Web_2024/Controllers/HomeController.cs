939cd7dc873d8323d9315cb7de8de5aba6a22437
SimonKpf <simonkapfinger05@gmail.com>
2024-02-23 12:30:50 +0100

    Contact und Informations hinzugefügt

--- KBS_FunEvents_Web_2024/Controllers/HomeController.cs
+++ KBS_FunEvents_Web_2024/Controllers/HomeController.cs
@@ -28,6 +28,17 @@ namespace KBS_FunEvents_Web_2024.Controllers
             return View();
         }
 
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Informations()
+        {
+            return View();
+        }
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
