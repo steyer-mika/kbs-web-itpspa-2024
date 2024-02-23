9badfa505c8b466e07b9bb9e68932cf7d927cb68
xXNawrotXx <maxi.nawrot@gmail.com>
2024-02-23 12:35:26 +0100

    Dashboardcontroller weitergemacht

--- KBS_FunEvents_Web_2024/Controllers/DashboardController.cs
+++ KBS_FunEvents_Web_2024/Controllers/DashboardController.cs
@@ -4,21 +4,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using KBS_FunEvents_Web_2024.Models;
 
 namespace KBS_FunEvents_Web_2024.Controllers
 {
     public class DashboardController : Controller
     {
         private ILogger<DashboardController> _logger;
+        private kbsContext kbsContext;
+        private int _kundenId;
 
-        public DashboardController(ILogger<DashboardController> pLogger)
+        public DashboardController(ILogger<DashboardController> pLogger, kbsContext dbContext)
         {
             _logger = pLogger;
+            kbsContext = dbContext;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult GetActiveBookings()
+        {
+            return View(kbsContext.TblBuchungens.Where(x => x.KdKundenId == _kundenId));
         }
     }
 }
