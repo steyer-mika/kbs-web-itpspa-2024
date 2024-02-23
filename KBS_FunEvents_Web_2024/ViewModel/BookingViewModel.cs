5af1c348ec450f97a1278eaa44eec608f7669829
Mika Steyer <mikasteyer9@gmail.com>
2024-02-23 12:09:24 +0100

    feat(booking): add basic booking view model

---
+++ KBS_FunEvents_Web_2024/ViewModel/BookingViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KBS_FunEvents_Web_2024.ViewModel
+{
+    public class BookingViewModel
+    {
+        [Key]
+        [Display(Name = "Buchungsnummer")]
+        public int BuBuchungsId { get; set; }
+
+        [Display(Name = "Kundennummer")]
+        public int KdKundenId { get; set; }
+
+        [Display(Name = "EventDatenId")]
+        public int EdEvDatenId { get; set; }
+
+        [Display(Name = "Gebuchte Plaetze")]
+        public int BuGebuchtePlaetze { get; set; }
+
+        [Display(Name = "bezahlt")]
+        public bool BuBezahlt { get; set; }
+
+        [Display(Name = "storniert")]
+        public bool BuStorniert { get; set; }
+
+        [Display(Name = "Rechnung erstellt am")]
+        public bool BuRechnungErstellt { get; set; }
+    }
+}
