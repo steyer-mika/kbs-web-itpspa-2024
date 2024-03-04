6ded7883e64b6925fb1a9175850b9d9f48a39d42
Mika Steyer <mikasteyer9@gmail.com>
2024-03-04 13:59:30 +0100

    fix: add correct model

--- KBS_FunEvents_Web_2024/ViewModel/BookingViewModel.cs
+++ KBS_FunEvents_Web_2024/ViewModel/BookingViewModel.cs
@@ -4,29 +4,40 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using KBS_FunEvents_Web_2024.Models;
 
 namespace KBS_FunEvents_Web_2024.ViewModel
 {
     public class BookingViewModel
     {
         [Key]
-        public int BuBuchungsId { get; set; }
-
-        [Display(Name = "Kundennummer")]
-        public int KdKundenId { get; set; }
-
         public int EdEvDatenId { get; set; }
 
-        [Display(Name = "Gebuchte Plaetze")]
-        public int BuGebuchtePlaetze { get; set; }
+        public int EtEventId { get; set; }
 
-        [Display(Name = "bezahlt")]
-        public bool BuBezahlt { get; set; }
+        [Display(Name = "Preis")]
+        public decimal EdPreis { get; set; }
 
-        [Display(Name = "storniert")]
-        public bool BuStorniert { get; set; }
+        [Display(Name = "Beginn")]
+        public DateTime EdBeginn { get; set; }
 
-        [Display(Name = "Rechnung erstellt am")]
-        public bool BuRechnungErstellt { get; set; }
+        [Display(Name = "Ende")]
+        public DateTime EdEnde { get; set; }
+
+        [Display(Name = "Startort")]
+        public string EdStartOrt { get; set; }
+
+        [Display(Name = "Zielort")]
+        public string EdZielort { get; set; }
+
+        public int EdMaxTeilnehmer { get; set; }
+
+        public int EdAktTeilnehmer { get; set; }
+
+        public bool EdFreigegeben { get; set; }
+
+        public decimal EdRabatt { get; set; }
+
+        public bool EdVeranstalterBenachrichtigt { get; set; }
     }
 }
