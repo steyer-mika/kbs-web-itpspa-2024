df230d147025864a6839c419a214caa8747fbb99
unknown <21ITC003@itbspa.de>
2024-03-05 12:39:45 +0100

    Dashboard Index View erstellt

---
+++ KBS_FunEvents_Web_2024/ViewModels/DashboardViewModels.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KBS_FunEvents_Web_2024.ViewModels
+{
+    public class DashboardViewModels
+    {
+    }
+}
