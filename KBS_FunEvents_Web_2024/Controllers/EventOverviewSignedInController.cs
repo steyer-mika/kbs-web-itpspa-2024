8f0a0e53c489d5f53d926efc5657c6e39179b939
unknown <21ITC007@itbspa.de>
2024-03-05 15:21:25 +0100

    feat: get data from database (wip)

--- KBS_FunEvents_Web_2024/Controllers/EventOverviewSignedInController.cs
+++ KBS_FunEvents_Web_2024/Controllers/EventOverviewSignedInController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using KBS_FunEvents_Web_2024.Models;
-using KBS_FunEvents_Web_2024.ViewModel;
+using KBS_FunEvents_Web_2024.ViewModels;
 
 namespace KBS_FunEvents_Web_2024.Controllers
 {
@@ -22,15 +22,33 @@ namespace KBS_FunEvents_Web_2024.Controllers
         // GET: EventViewModels
         public async Task<IActionResult> Index()
         {
-            return View(await _context.EventViewModel.ToListAsync());
+
+            var vm = GetData();
+
+            return View("Index", vm);
         }
 
         // GET: EventViewModels/Details/5
 
-
-        private bool EventViewModelExists(int id)
+        private List<EventOverviewViewModel> GetData()
         {
-            return _context.EventViewModel.Any(e => e.EtEventId == id);
+            var data = _context.TblEvents;
+
+            List<EventOverviewViewModel> dataForVM = new List<EventOverviewViewModel>();
+
+            foreach (var eventData in data)
+            {
+                EventOverviewViewModel ev = new EventOverviewViewModel
+                {
+                    EtEventId = eventData.EtEventId,
+                    EtBezeichnung = eventData.EtBezeichnung,
+                };
+
+                dataForVM.Add(ev);
+            }
+
+            return dataForVM;
+
         }
     }
 }
--- KBS_FunEvents_Web_2024/Models/kbsContext.cs
+++ KBS_FunEvents_Web_2024/Models/kbsContext.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
-using KBS_FunEvents_Web_2024.ViewModel;
+using KBS_FunEvents_Web_2024.ViewModels;
 
 #nullable disable
 
@@ -100,6 +100,6 @@ namespace KBS_FunEvents_Web_2024.Models
 
         partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 
-        public DbSet<KBS_FunEvents_Web_2024.ViewModel.EventViewModel> EventViewModel { get; set; }
+        //public DbSet<EventOverviewViewModel> EventViewModel { get; set; }
     }
 }
