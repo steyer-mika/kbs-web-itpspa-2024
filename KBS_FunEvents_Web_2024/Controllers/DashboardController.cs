b120ad85571b6d3b1e7d66653fbbd8c2fe295942
Mika Steyer <mikasteyer9@gmail.com>
2024-03-05 12:02:18 +0100

    feat(booking): add booking model view data

--- KBS_FunEvents_Web_2024/Controllers/DashboardController.cs
+++ KBS_FunEvents_Web_2024/Controllers/DashboardController.cs
@@ -4,16 +4,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using KBS_FunEvents_Web_2024.Models;
+using KBS_FunEvents_Web_2024.ViewModel;
 
 namespace KBS_FunEvents_Web_2024.Controllers
 {
     public class DashboardController : Controller
     {
         private ILogger<DashboardController> _logger;
+        private readonly Models.kbsContext _dbContext;
 
-        public DashboardController(ILogger<DashboardController> pLogger)
+        public DashboardController(ILogger<DashboardController> pLogger, Models.kbsContext dbContext)
         {
             _logger = pLogger;
+            _dbContext = dbContext;
+
         }
 
         public IActionResult Index()
@@ -21,9 +26,31 @@ namespace KBS_FunEvents_Web_2024.Controllers
             return View();
         }
 
-        public IActionResult Booking()
+        public async Task<IActionResult> Booking(int id)
         {
-            return View("Booking");
+            TblEventDaten eventDaten = await _dbContext.TblEventDatens.FindAsync(id);
+
+            if (eventDaten == null)
+            {
+                return View();
+            }
+
+            BookingViewModel bvm = new();
+
+            bvm.EdEvDatenId = eventDaten.EdEvDatenId;
+            bvm.EtEventId = eventDaten.EtEventId;
+            bvm.EdPreis = eventDaten.EdPreis;
+            bvm.EdBeginn = eventDaten.EdBeginn;
+            bvm.EdEnde = eventDaten.EdEnde;
+            bvm.EdStartOrt = eventDaten.EdStartOrt;
+            bvm.EdZielort = eventDaten.EdZielort;
+            bvm.EdMaxTeilnehmer = eventDaten.EdMaxTeilnehmer;
+            bvm.EdAktTeilnehmer = eventDaten.EdAktTeilnehmer;
+            bvm.EdFreigegeben = eventDaten.EdFreigegeben;
+            bvm.EdRabatt = eventDaten.EdRabatt;
+            bvm.EdVeranstalterBenachrichtigt = eventDaten.EdVeranstalterBenachrichtigt;
+
+            return View("Booking", bvm);
         }
     }
 }
