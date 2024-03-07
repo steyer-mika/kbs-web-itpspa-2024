0f4621f72a965b75be69e23a5ef3452cca3a828f
BraunT1 <160847650+BraunT1@users.noreply.github.com>
2024-03-07 12:24:48 +0100

    Update DashboardController.cs

--- KBS_FunEvents_Web_2024/Controllers/DashboardController.cs
+++ KBS_FunEvents_Web_2024/Controllers/DashboardController.cs
@@ -23,7 +23,22 @@ namespace KBS_FunEvents_Web_2024.Controllers
 
         public IActionResult Index()
         {
-            return View();
+                        TblEventDaten eventDaten = _kbsContext.TblEventDatens.Find(id);
+            
+            if (eventDaten == null)
+            {
+                return View();
+            }
+
+            TblEvent baseEvent = _kbsContext.TblEvents.Find(eventDaten.EtEventId);
+            
+            DashboardModelView mv = new DashboardModelView();
+
+            mv.EdBeginn = eventDaten.EdBeginn;
+            mv.EtBeschreibung = baseEvent.EtBeschreibung;
+            mv.EtBezeichnung = baseEvent.EtBezeichnung;
+
+            return View(mv);
         }
 
         public IActionResult Dashboard(int id)
