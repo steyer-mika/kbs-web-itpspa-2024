c238ae91236d0c57b15ab562505757a2f15ce759
BraunT1 <160847650+BraunT1@users.noreply.github.com>
2024-03-07 12:18:37 +0100

    Update DashboardController.cs

--- KBS_FunEvents_Web_2024/Controllers/DashboardController.cs
+++ KBS_FunEvents_Web_2024/Controllers/DashboardController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using KBS_FunEvents_Web_2024.Models;
+using Microsoft.EntityFrameworkCore;
+using KBS_FunEvents_Web_2024.ViewModels;
 
 namespace KBS_FunEvents_Web_2024.Controllers
 {
